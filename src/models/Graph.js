c56255300e70ae1aec155644416986928ccd1e2c
Amin Limbada <amin.limbada@consensys.net>
2020-08-06 03:56:21 +0400

    Create PossibleRoutes component

---
+++ src/models/Graph.js
@@ -0,0 +1,87 @@
+export default class Graph {
+  constructor() {
+    this.numberOfNodes = 0;
+    this.adjacentList = {};
+  }
+  addVertex(node) {
+    this.adjacentList[node] = [];
+    this.numberOfNodes++;
+  }
+  addEdge(node1, node2) {
+    //directed graph
+    this.adjacentList[node1].push(node2);
+  }
+  showConnections() {
+    const allNodes = Object.keys(this.adjacentList);
+    for (let node of allNodes) {
+      let nodeConnections = this.adjacentList[node];
+      let connections = "";
+      let vertex;
+      for (vertex of nodeConnections) {
+        connections += vertex + " ";
+      }
+      console.log(node + "-->" + connections);
+    }
+  }
+
+  // getPossibleRoutesBFS = (start, end) => {
+  //   const queue = [start];
+  //   const visited = new Set();
+  //   let routes = [];
+  //   let count = 0;
+  //   while (queue.length > 0) {
+  //     const fromTown = queue.shift()["to"];
+  //     //routes.push(fromTown);
+  //     const destinations = this.adjacentList[fromTown];
+
+  //     if (destinations) {
+  //       for (const town of destinations) {
+  //         if (town.to === end) {
+  //           console.log(routes);
+  //           //routes = [];
+  //           count++;
+  //         }
+
+  //         if (!visited.has(town.to)) {
+  //           visited.add(town.to);
+  //           queue.push(town);
+  //         }
+  //       }
+  //     }
+  //   }
+  //   console.log(count);
+  // };
+  getPossibleRoutesBFS = (start, end) => {
+    let result = [];
+    let queue = [[start.to]];
+    try {
+      console.log(this.adjacentList[start.to]);
+      if (!this.adjacentList[start.to]) {
+        throw Error("The start route does not exist");
+      }
+      if (!this.adjacentList[end]) {
+        throw Error("The end route does not exist");
+      }
+      while (queue.length) {
+        let paths = queue.shift();
+        let lastNode = paths[paths.length - 1];
+        if (lastNode === end) {
+          result.push([paths]);
+        } else {
+          let neighbors = this.adjacentList[lastNode];
+
+          if (neighbors) {
+            for (let neighbor of neighbors) {
+              let list = [...paths, ...neighbor.to];
+              queue.push(list);
+            }
+          }
+        }
+      }
+    } catch (error) {
+      return { error: true, text: error };
+    }
+
+    return result;
+  };
+}
---
+++ src/scenes/PossibleRoutes/component.jsx
@@ -0,0 +1,130 @@
+import React, { useState } from "react";
+import Graph from "../../models/Graph";
+import "./styles.css";
+
+const PossibleRoutes = (props) => {
+  const [paths, setPaths] = useState("");
+  const [source, setSournce] = useState("");
+  const [target, setTarget] = useState("");
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
+  const myGraph = new Graph();
+
+  const populateGraph = (arr) => {
+    for (let value of arr) {
+      if (!(value.charAt(0) in myGraph.adjacentList)) {
+        myGraph.addVertex(value.charAt(0));
+      }
+      myGraph.addEdge(value.charAt(0), {
+        to: value.charAt(1),
+        distance: parseInt(value.slice(2), 10),
+      });
+    }
+  };
+
+  const handleClick = async (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    populateGraph(paths.split(","));
+    let routesResult = myGraph.getPossibleRoutesBFS({ to: source }, target);
+    if (!routesResult.error) {
+      setResult(routesResult);
+    } else setError(routesResult.text.message);
+  };
+  return (
+    <div>
+      <form className="Form" onSubmit={handleClick}>
+        <div>
+          <label
+            htmlFor="paths"
+            className="Label"
+            data-testid="routes__label__paths"
+          >
+            Paths:
+          </label>
+          <br></br>
+          <input
+            type="text"
+            onChange={(e) =>
+              setPaths(e.target.value.toUpperCase().replace(/\s/g, ""))
+            }
+            value={paths}
+            id="paths"
+            className="Input"
+            required
+            pattern="([a-zA-Z]{2}\d+(,||\s))+"
+            title="Please enter text in the form of AB1,BC2,CD3"
+            data-testid="routes__input__paths"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="source"
+            className="Label"
+            data-testid="routes__label__source"
+          >
+            Source:
+          </label>
+          <br></br>
+          <input
+            type="source"
+            onChange={(e) =>
+              setSournce(e.target.value.toUpperCase().replace(/\s/g, ""))
+            }
+            value={source}
+            id="paths"
+            className="Input"
+            required
+            pattern="[a-zA-Z]{1}"
+            title="Please enter a letter"
+            data-testid="routes__label__source"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="target"
+            className="Label"
+            data-testid="routes__label__target"
+          >
+            Target:
+          </label>
+          <br></br>
+          <input
+            type="text"
+            onChange={(e) =>
+              setTarget(e.target.value.toUpperCase().replace(/\s/g, ""))
+            }
+            value={target}
+            id="target"
+            className="Input"
+            required
+            pattern="[a-zA-Z]{1}"
+            title="Please enter a letter"
+            data-testid="routes__input__target"
+          />
+        </div>
+        <input
+          type="submit"
+          value="Calculate"
+          className="btn"
+          data-testid="routes__button__calculate"
+        />
+      </form>
+      {result.length && !error && (
+        <div className="Result" data-testid="routes__text__result">
+          <p>Routes found: {result.length}</p>
+          {result.map((res) =>
+            res.map((route, index) => <p key={index}>{route}</p>)
+          )}
+        </div>
+      )}
+      {error && (
+        <p className="Error" data-testid="routes__text__error">
+          {error}
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default PossibleRoutes;
